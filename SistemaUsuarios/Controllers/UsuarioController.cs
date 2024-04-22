3eb752751460443233fa4417bdcd7b7e98da4d88
cauamicael <cauamicael@hotmail.com>
2024-04-22 20:25:30 -0300

    (feat): Creation of the front on the screens Listagem de usuarios, Cadastro, Editar e Apagar usuario

---
+++ SistemaUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaUsuarios.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
